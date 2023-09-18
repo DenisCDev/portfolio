dfc25c1f5c0b90b9056392a8f501e000500b4b54
IntLab4 <intlabs@proton.me>
2023-09-17 22:31:56 -0300

    fix: icons e mensagem js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
- function mostrarMensagem() {
-              alert("Mensagem enviada com sucesso :D");
-            }
+function mostrarMensagem() {
+  alert("Mensagem enviada com sucesso! Obrigado");
+}
 
 console.log("Bem vindo(a) ao meu portfólio, espero que goste :)")
